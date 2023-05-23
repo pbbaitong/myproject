7781b653caed01d78b16082d3d11355e886b4603
pbbaitong <113036385+pbbaitong@users.noreply.github.com>
2023-05-23 16:51:32 +0700

    Update utils.py

--- streamlit_login_auth/utils.py
+++ streamlit_login_auth/utils.py
@@ -51,7 +51,7 @@ def check_usr_role(tel_login: str):
         # general = mycursor.execute("SELECT * FROM general WHERE general_firstname = %s AND general_lastname = %s", (firstname_login, lastname_login))
         # general = session.execute("SELECT * FROM general WHERE general_tel = %s", (tel_login,))
         # text_query = "SELECT * FROM general WHERE general_tel = (:s);"
-        general = session.execute("SELECT * FROM general WHERE general_tel = %s", (tel_login,))
+        general = session.execute("SELECT * FROM general WHERE general_tel = (:s);", {"s":tel_login})
         general_existed = session.fetchone()
         # st.write(general_existed)
         session.commit()
